0fcf9fc6806ba4e2cf374a2f5c1bc8b71060d67b
matheustsx <matheusbatistachavante@gmail.com>
2024-11-07 22:46:53 -0300

    feat: criando o ambiente do projeto

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { ContentComponent } from "./components/content/content.component";
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, ContentComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'angular-weather';
+}
